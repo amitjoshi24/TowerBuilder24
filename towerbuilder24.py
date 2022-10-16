8d7fa8045e5632bca9d8cdfa78ec43c053745d2f
= <amit.joshiusa@gmail.com>
2022-10-16 16:03:47 -0500

    cleaned up code

--- towerbuilder24.py
+++ towerbuilder24.py
@@ -6,9 +6,6 @@ karr = {int(amit) for amit in input("Enter block heights, separated by a space: 
 
 n = len(karr)
 
-dp = [False] * (h+1)
-backtrack = [-1] * (h+1)
-
 from collections import deque, defaultdict
 
 nextValues = deque()
@@ -19,18 +16,18 @@ backtracked = defaultdict(lambda: -1)
 visited = set()
 
 for ki in karr:
-
 	nextValues.append(ki)
 	fromValues.append(0)
 
 foundSolution = False
 while len(nextValues) > 0:
-
+	#print (len(backtracked))
 	cur = nextValues.popleft()
+	
 	fromVal = fromValues.popleft()
 	if cur in visited or cur > h:
 		continue
-
+	
 	visited.add(cur)
 	backtracked[cur] = fromVal
 
